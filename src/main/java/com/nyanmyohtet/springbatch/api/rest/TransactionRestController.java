71d817eab4f5ad4426c5a27f4c68f1a135d17cac
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2024-09-03 21:22:01 +0700

    refactor TransactionRestController

--- src/main/java/com/nyanmyohtet/springbatch/api/rest/TransactionRestController.java
+++ src/main/java/com/nyanmyohtet/springbatch/api/rest/TransactionRestController.java
@@ -2,7 +2,6 @@ package com.nyanmyohtet.springbatch.api.rest;
 
 import com.nyanmyohtet.springbatch.api.rest.request.UpdateDescriptionRequest;
 import com.nyanmyohtet.springbatch.api.rest.response.SearchTransactionResponse;
-import com.nyanmyohtet.springbatch.exception.ResourceNotFoundException;
 import com.nyanmyohtet.springbatch.persistence.model.Transaction;
 import com.nyanmyohtet.springbatch.service.TransactionService;
 import com.nyanmyohtet.springbatch.util.AppConstants;
@@ -12,8 +11,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ConcurrentModificationException;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,45 +32,27 @@ public class TransactionRestController {
             @RequestParam(defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
         LOG.info("searchTransaction with params: customerId={}, accountNumber={}, description={}",
                 customerId, accountNumber, description);
-        try {
-            SearchTransactionResponse response = transactionService.searchTransaction(
-                    customerId, accountNumber, description, pageNo, pageSize, sortBy, sortDir);
 
-            if (response.getContent().isEmpty()) {
-                LOG.info("No transactions found for the provided criteria.");
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+        SearchTransactionResponse response = transactionService.searchTransaction(
+                customerId, accountNumber, description, pageNo, pageSize, sortBy, sortDir);
 
-            return new ResponseEntity<>(response, HttpStatus.OK);
-
-        } catch (Exception ex) {
-            String message = "Error occurred while searching transactions";
-            LOG.error(message, ex);
-            throw new RuntimeException(message);
+        if (response.getContent().isEmpty()) {
+            LOG.info("No transactions found for the provided criteria.");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PatchMapping("/{transactionId}")
-    public ResponseEntity<?> updateDescription(
+    public ResponseEntity<Transaction> updateDescription(
             @PathVariable Long transactionId,
             @RequestBody UpdateDescriptionRequest updateDescriptionRequest) {
 
         LOG.info("Attempting to update description for transaction with ID: {} and requestBody: {}", transactionId, updateDescriptionRequest);
 
-        try {
-            Transaction updatedTransaction = transactionService.updateDescription(transactionId, updateDescriptionRequest);
-            LOG.info("Successfully updated description for transaction with ID: {}", transactionId);
-            return ResponseEntity.ok(updatedTransaction);
-        } catch (ResourceNotFoundException ex) {
-            LOG.error("Transaction not found with ID: {}", transactionId, ex);
-            throw ex;
-        } catch (ConcurrentModificationException ex) {
-            LOG.error("Concurrent modification error for transaction with ID: {}", transactionId, ex);
-            throw ex;
-        } catch (Exception ex) {
-            String message = "An unexpected error occurred while updating the transaction with ID: " + transactionId;
-            LOG.error(message, ex);
-            throw new RuntimeException(message);
-        }
+        Transaction updatedTransaction = transactionService.updateDescription(transactionId, updateDescriptionRequest);
+        LOG.info("Successfully updated description for transaction with ID: {}", transactionId);
+        return ResponseEntity.ok(updatedTransaction);
     }
 }
