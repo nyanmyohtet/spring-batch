0faa64408892d6ad9b1437986972c6b7d7189c61
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2025-05-06 16:24:02 +0800

    run job from command line runner

--- src/main/java/com/nyanmyohtet/springbatch/SpringBatchApplication.java
+++ src/main/java/com/nyanmyohtet/springbatch/SpringBatchApplication.java
@@ -1,13 +1,33 @@
 package com.nyanmyohtet.springbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchApplication {
+public class SpringBatchApplication implements CommandLineRunner {
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job sampleJob;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		JobParameters params = new JobParametersBuilder()
+				.addLong("run.id", System.currentTimeMillis())
+				.toJobParameters();
+
+		jobLauncher.run(sampleJob, params);
+	}
 }
